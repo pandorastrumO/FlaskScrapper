d30fad5ec9798d7fabdb7a45307c5e499986318f
PandorAstrum <primeintegerslab@gmail.com>
2018-09-12 01:39:53 +0600

    Flask setup with Scrapper

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask import jsonify
+from scrapping import scrape
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Aldrin Scrapper'
+def index():
+    return render_template('index.html')
 
+@app.route('/results')
+def results():
+    return render_template('results.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=5000)
